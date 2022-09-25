c71bb57afaca60b1892aba5fe5947343ca3a7ab6
西索酱 <takegine@vip.qq.com>
2022-09-25 16:43:14 +0800

    patch version to 1.1.28

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,28 @@
 import { exec } from 'child_process'
+import { readFileSync, writeFileSync } from 'fs'
+import { resolve } from 'path'
 import chalk from 'chalk'
 import getVersion from './getVersion'
-import fs from 'fs'
-import path from 'path'
 
 const { cyan, green, red } = chalk
 
-export default async function(options) {
+export default async function(options:any) {
+  const [type, branch = 'master'] = options.args
+  console.info(type, branch, options.args)
+  const { projectVersion, projectName } = await getVersion()
+  console.info(green(`Start to ${type} version to ${projectName}...`))
+  const newVersion = getNewVersion(projectVersion)
+  writeNewVersion()
+  console.info(green(`\nVersion: ${cyan(`${projectVersion} -> ${newVersion}`)}`))
+  console.info(green(`${type} ${projectName} version to ${newVersion}`))
+  await execShell()
+  console.info(`\n${green('[ Cimi ]')} Release ${projectName} Success!\n`)
+
   //获取新的版本号
-  function getNewVersion(oldVersion) {
+  function getNewVersion(oldVersion:string) {
     let [major, minor, patch] = oldVersion.split('.')
     if(patch.length > 2 && patch.includes('-beta')) {
-      patch = Number(patch.split('-')[0]);
+      patch = patch.split('-')[0];
     }
     switch (type) {
       case 'patch':
@@ -35,64 +46,61 @@ export default async function(options) {
   }
   //写入新版本号，更新项目文件
   function writeNewVersion() {
-    const packageJson = fs.readFileSync(
-      path.resolve(process.cwd(), 'package.json'),
+    const packageJson = readFileSync(
+      resolve(process.cwd(), 'package.json'),
       'utf8'
     )
     const newPackageJson = packageJson.replace(
       `"version": "${projectVersion}"`,
       `"version": "${newVersion}"`
     )
-    fs.writeFileSync(
-      path.resolve(process.cwd(), 'package.json'),
+    writeFileSync(
+      resolve(process.cwd(), 'package.json'),
       newPackageJson
     )
-    console.log(green('\nUpdate package.json success!'))
+    console.info(green('\nUpdate package.json success!'))
   }
   //执行整个流程的命令
-  function execShell() {
-    const shellList = [
-      `echo "\n${green('[ 1 / 3 ]')} ${cyan(
-        `Commit and push to ${branch} branch`
-      )}\n"`,
+  async function execShell() {
+    const echo1 = `${green('[ 1 / 3 ]')} ${cyan(`Commit and push to ${branch} branch`)}`
+    const part1 = [
       'git add .',
       `git commit -m "${type} version to ${newVersion}"`,
       `git push origin ${branch}`,
-      `echo "\n${green('[ 2 / 3 ]')} ${cyan(
-        `Tag and push tag to ${branch}`
-      )}\n"`,
+    ]
+    const echo2 = `${green('[ 2 / 3 ]')} ${cyan(`Tag and push tag to ${branch}`)}`
+    const part2 = [
       `git tag ${newVersion}`,
       `git push origin ${newVersion}`,
-      `echo "\n${green('[ 3 / 3 ]')} ${cyan('Publish to NPM')}\n"`,
+    ]
+    const echo3 = `${green('[ 3 / 3 ]')} ${cyan('Publish to NPM')}`
+    const part3 = [
       `npm publish ${options.accessPublic ? '--access=public' : ''}`,
-    ].join(' && ')
-    return new Promise((resolve) => {
+    ]
+    await step(echo1, part1)
+    await step(echo2, part2)
+    await step(echo3, part3)
+  }
+
+  async function step(desc: string, command: string[]) {
+    console.log(desc)
+    return new Promise((resolve, reject) => {
       const childExec = exec(
-        shellList,
+        command.join(' && '),
         { maxBuffer: 10000 * 10240 },
-        (err, stdout) => {
+        (err, stdout, stderr) => {
+          console.log(err, stdout, stderr)
           if (err) {
-            console.log(err)
+            reject(err)
             throw err
           } else {
             resolve('')
           }
         }
       )
-      childExec.stdout.pipe(process.stdout)
-      childExec.stderr.pipe(process.stderr)
+      childExec.stdout?.pipe(process.stdout)
+      childExec.stderr?.pipe(process.stderr)
     })
   }
-
-  const [type, branch = 'master'] = options.args
-  console.log(type, branch, options.args)
-  const { projectVersion, projectName } = await getVersion()
-  console.log(green(`Start to ${type} version to ${projectName}...`))
-  const newVersion = getNewVersion(projectVersion)
-  writeNewVersion()
-  console.log(green(`\nVersion: ${cyan(`${projectVersion} -> ${newVersion}`)}`))
-  console.log(green(`${type} ${projectName} version to ${newVersion}`))
-  await execShell()
-  console.log(`\n${green('[ Cimi ]')} Release ${projectName} Success!\n`)
 }
 
