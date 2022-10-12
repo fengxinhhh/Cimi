7da8fa637d3b0fb94ece9953d0489653da1de07e
zzall <1809642355@qq.com>
2022-10-12 20:48:04 +0800

    chore:prettier匹配规则

--- getVersion.ts
+++ getVersion.ts
@@ -1,13 +1,13 @@
-const fs = require('fs')
-const path = require('path')
+const fs = require('fs');
+const path = require('path');
 
-module.exports = async function() {
+module.exports = async function () {
   const packageJson = fs.readFileSync(
     path.resolve(process.cwd(), 'package.json'),
-    'utf8'
-  )
+    'utf8',
+  );
   return {
     projectVersion: JSON.parse(packageJson).version,
     projectName: JSON.parse(packageJson).name,
-  }
-}
+  };
+};
