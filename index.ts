13a2e5324e1ddfd900a9dd9e6afcdbf940958201
zzall <1809642355@qq.com>
2022-10-12 12:11:05 +0800

    fix:tsc type error

--- index.ts
+++ index.ts
@@ -4,7 +4,7 @@ const { resolve } = require('path');
 const { green, red, cyan } = require('chalk');
 const getVersion = require('./getVersion.ts');
 const inquirer = require('inquirer');
-
+export {};
 module.exports = async function (options) {
 	const type = options.rawArgs[2];
 	const branch = options.rawArgs[3] || 'master';
