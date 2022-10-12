5a81a4d18cbdf2d43af2f207867104fbaaacdf82
Xin Feng <1244200081@qq.com>
2022-10-12 22:19:21 +0800

    patch version to 1.1.42

--- bin/cli.ts
+++ bin/cli.ts
@@ -10,13 +10,13 @@ program
   .description(
     'A fully automatic NPM package tool, one line command to help you "git replase, create git tag, release NPM package"'
   )
-  .version(pkg.version, "-v, --version")
-  .option("manual", "manual select your new package version")
-  .option("patch", "patch your new npm package")
-  .option("minor", "minor your new npm package")
-  .option("major", "major your new npm package")
-  .option("beta", "beta your new npm package")
-  .option("upgradeBeta", "upgrade your new npm package")
+  .version(pkg.version, '-v, --version')
+  .option('manual', 'manual select your new package version')
+  .option('patch', 'patch your new npm package')
+  .option('minor', 'minor your new npm package')
+  .option('major', 'major your new npm package')
+  .option('beta', 'beta your new npm package')
+  .option('upgradeBeta', 'upgrade your new npm package')
   // .option("patchBeta", "patch your new beta npm package")
   // .option("minorBeta", "minor your new beta npm package")
   // .option("majorBeta", "major your new beta npm package")
@@ -24,18 +24,18 @@ program
     console.log('\n  Tip:\n');
     console.log(
       '    You should run this script in the root directory of you project or run by npm scripts.'
-    )
-    console.log('\n  Examples:\n')
-    console.log(`    ${green('$')} cimi manual [branch] (default: master)`)
-    console.log(`    ${green('$')} cimi patch [branch] (default: master)`)
-    console.log(`    ${green('$')} cimi minor [branch] (default: master)`)
-    console.log(`    ${green('$')} cimi major [branch] (default: master)`)
-    console.log(`    ${green('$')} cimi beta [branch] (default: master)`)
-    console.log(`    ${green('$')} cimi upgradeBeta [branch] (default: master)`)
+    );
+    console.log('\n  Examples:\n');
+    console.log(`    ${green('$')} cimi manual [branch] (default: master)`);
+    console.log(`    ${green('$')} cimi patch [branch] (default: master)`);
+    console.log(`    ${green('$')} cimi minor [branch] (default: master)`);
+    console.log(`    ${green('$')} cimi major [branch] (default: master)`);
+    console.log(`    ${green('$')} cimi beta [branch] (default: master)`);
+    console.log(`    ${green('$')} cimi upgradeBeta [branch] (default: master)`);
     // console.log(`    ${green('$')} cimi patchBeta [branch] (default: master)`)
     // console.log(`    ${green('$')} cimi minorBeta [branch] (default: master)`)
     // console.log(`    ${green('$')} cimi majorBeta [branch] (default: master)`)
-    console.log('')
+    console.log('');
   })
   .parse(process.argv);
 
