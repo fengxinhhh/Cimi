9ccc5a758868a40c9aeae8de02322901e7a36390
Xin Feng <1244200081@qq.com>
2022-10-12 22:31:00 +0800

    update: cimi type error info

--- index.ts
+++ index.ts
@@ -9,12 +9,12 @@ module.exports = async function (options) {
   const type = options.rawArgs[2];
   const branch = options.rawArgs[3] || 'master';
   // console.log(options)
-  console.info(`Cimi type: ${green(type)}, Cimi push branch: ${green(branch)}`)
-  const { projectVersion, projectName } = await getVersion()
+  console.info(`Cimi type: ${green(type)}, Cimi push branch: ${green(branch)}`);
+  const { projectVersion, projectName } = await getVersion();
   if (type) {
-    console.info(green(`Start to ${type} version to ${projectName}...`))
+    console.info(green(`Start to ${type} version to ${projectName}...`));
   } else {
-    console.info(green(`Start to manual select new version to ${projectName}...`))
+    console.info(green(`Start to manual select new version to ${projectName}...`));
   }
   const newVersion = await getNewVersion(projectVersion);
   writeNewVersion();
@@ -25,7 +25,7 @@ module.exports = async function (options) {
 
   //获取新的版本号
   function getNewVersion(oldVersion) {
-    let [major, minor, patch] = oldVersion.split('.')
+    let [major, minor, patch] = oldVersion.split('.');
     const betaVersion = oldVersion?.split('beta')[1] || 1;
     if (patch.length > 2 && patch.includes('-beta')) {
       patch = patch.split('-')[0];
@@ -36,11 +36,11 @@ module.exports = async function (options) {
       case 'minor':
         return `${major}.${+minor + 1}.${patch}`;
       case 'major':
-        return `${+major + 1}.${minor}.${patch}`
+        return `${+major + 1}.${minor}.${patch}`;
       case 'beta':
-        return `${major}.${minor}.${patch}-beta`
+        return `${major}.${minor}.${patch}-beta`;
       case 'upgradeBeta':
-        return `${major}.${minor}.${patch}-beta${+betaVersion + 1}`
+        return `${major}.${minor}.${patch}-beta${+betaVersion + 1}`;
       // case 'patchBeta':
       //   return `${major}.${minor}.${+patch + 1}-beta`
       // case 'minorBeta':
@@ -51,9 +51,9 @@ module.exports = async function (options) {
         return inquirer
           .prompt([
             {
-              type: "list",
-              name: "cimiType",
-              message: "please select new version",
+              type: 'list',
+              name: 'cimiType',
+              message: 'please select new version',
               choices: [
                 `patch ${major}.${minor}.${+patch + 1}`,
                 // `patch-beta ${major}.${minor}.${+patch + 1}-beta`,
@@ -62,18 +62,18 @@ module.exports = async function (options) {
                 `major ${+major + 1}.${minor}.${patch}`,
                 // `major-beta ${+major + 1}.${minor}.${patch}-beta`,
                 `${major}.${minor}.${patch}-beta`,
-                `${major}.${minor}.${patch}-beta${+betaVersion + 1}`
+                `${major}.${minor}.${patch}-beta${+betaVersion + 1}`,
               ],
             },
           ])
-          .then((answers) => {
+          .then(answers => {
             try {
-              return answers["cimiType"].match(/(?<=\w+\s+)(\w|\.|\-)+/)[0];
+              return answers['cimiType'].match(/(?<=\w+\s+)(\w|\.|\-)+/)[0];
             } catch (err) {
-              return answers["cimiType"]
+              return answers['cimiType'];
             }
           })
-          .catch((error) => {
+          .catch(error => {
             if (error.isTtyError) {
               // Prompt couldn't be rendered in the current environment
               console.log(red(`Prompt couldn't be rendered in the current environment`));
@@ -84,9 +84,7 @@ module.exports = async function (options) {
       }
       default:
         console.error(
-          red(
-            '\nPlease write correctly update type: patch、minor、major、patchBeta、minorBeta、majorBeta\n'
-          )
+          red('\nPlease write correctly update type: patch、minor、major、beta、upgradeBeta\n')
         );
         process.exit(1);
     }
